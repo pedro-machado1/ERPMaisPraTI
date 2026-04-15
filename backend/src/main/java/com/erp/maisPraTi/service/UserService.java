ce01ee28becf080be84a6908300ad7157c3a399e
pedro-machado1 <pedro.jardim.machado@gmail.com>
2026-04-15 03:56:01 -0300

    fixing small bug

--- backend/src/main/java/com/erp/maisPraTi/service/UserService.java
+++ backend/src/main/java/com/erp/maisPraTi/service/UserService.java
@@ -11,6 +11,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,7 +26,7 @@ import static com.erp.maisPraTi.util.EntityMapper.convertToEntity;
 public class UserService {
 
     @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private UserRepository userRepository;
